a45dbd76c7cc2d49d94710b447d76ac118de0c6b
vasugit1 <35460130+vasugit1@users.noreply.github.com>
2025-12-23 12:43:24 -0600

    math update for the calcualtor.

--- AssetPanda/AssetPanda/Services/NetWorthCalculator.swift
+++ AssetPanda/AssetPanda/Services/NetWorthCalculator.swift
@@ -11,15 +11,22 @@ struct NetWorthCalculator {
         } else {
             principal = max(0, asset.currentValue)
         }
-        let years = Double(max(0, asset.investMonths)) / 12.0
+        let n = max(0, asset.investMonths)
         // If yieldRate is 0 or unset (treated as 0), use 0 in calculation
         let yieldRate = asset.yieldRate == 0 ? 0 : asset.yieldRate
         var effectiveRate = (asset.growthRate + yieldRate) / 100.0
         if let inflation = asset.inflation {
             effectiveRate -= inflation / 100.0
         }
-        let fv = principal * pow(1.0 + effectiveRate, years)
-        return fv
+        let r = effectiveRate
+        let M = asset.monthlyContribution
+        if r == 0 {
+            return principal + M * Double(n)
+        } else {
+            let monthlyRate = r / 12.0
+            let fv = principal * pow(1.0 + monthlyRate, Double(n)) + M * ( (pow(1.0 + monthlyRate, Double(n)) - 1) / monthlyRate )
+            return fv
+        }
     }
     /// Calculates the rounded total future value for an array of assets.
     static func calculateFutureValue(for assets: [Asset]) -> Double {
